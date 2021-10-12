12943a8c9773ae2d909d839d77a51bce8e24bcd7
Benjono <56357931+Benjono@users.noreply.github.com>
2021-10-12 22:13:47 +0100

    Improvements to comments + functionality
    Significantly improved comments, renamed variables for clarity, added functionality with stablePreWork function so that 'magic numbers' doesn't have to be experienced by the user.

--- stableMatch.py
+++ stableMatch.py
@@ -1,47 +1,81 @@
 def stableMatch(aPref, bPref):
-    freeA = [i for i in range(0, len(aPref))]
     #all free to begin with
-    freeAmatch = [0 for i in range(0,len(aPref))]
-    freeB = [i for i in range(0, len(bPref))]
+    freeInA = [i for i in range(0, len(aPref))]
+    freeInAmatch = [0 for i in range(0,len(aPref))]
+    freeInB = [i for i in range(0, len(bPref))]
     pairs = []
-    while(len(freeA)>0):
-        i = freeA[0]
-        if freeAmatch[i]==len(aPref[i]):
-            freeA.remove(i)
+    while(len(freeInA)>0):
+        i = freeInA[0]
+        #If no more matches can be made for them
+        if freeInAmatch[i]==len(aPref[i]): 
+            #Remove from those that are free
+            freeInA.remove(i) 
         else:
-            topAmatch= aPref[i][freeAmatch[i]]
-            foundMatch = True
-            if topAmatch in freeB:
-                #aPref[i] is preferences of a
-                #aPref[i][freeAmatch[i]] is B, or the next best partner of a according to preferences
+            #What is their top match
+            topAmatch = aPref[i][freeInAmatch[i]]
+            #Is their top match free?
+            if topAmatch in freeInB:
+                #Then pair them
                 pairs.append([i,topAmatch])
-                #Add the pair i and i's preference
-                freeB.remove(topAmatch)
-                freeA.remove(i)
+                #Both aren't free
+                freeInB.remove(topAmatch)
+                freeInA.remove(i)
             else:
+                #For each pair
                 for pair in pairs:
+                    #And there is a pairing where it is preferential
+                    #for A
                     if topAmatch==pair[1]:
-                        #if there is a pairing with this partner
-                        #aPref[i] is the preferences of a
-                        #aPref[i][freeAmatch[i]] is B, or the next best partner of a according to preferences
-                        #pair[1] is 
+                        #If it is preferential for B also
                         if bPref[pair[1]].index(pair[0]) > bPref[pair[1]].index(i):
-                            #freeB[pair[1]] is the current partners preferences
-                            #.index(pair[0]) is ranking of current partner, a'
-                            #.index(i) is ranking of proposed partner, a
-                            #then if B prefers a
-                            freeA.append(pair[0]) #a' will be free soon
-                            pairs.remove(pair) #a' pair will be removed
-                            pairs.append([i,topAmatch]) #add pair [i, a]
-                            freeA.remove(i) #remove A from being free
-            freeAmatch[i] = freeAmatch[i]+1
+                            #change pairing
+                            freeInA.append(pair[0]) 
+                            pairs.remove(pair)
+                            pairs.append([i,topAmatch]) 
+                            freeInA.remove(i)
+            freeInAmatch[i] = freeInAmatch[i]+1
             #regardless of if the pairings change, go to the next preference      
     return pairs
+
+def stablePreWork(leftNames, rightNames):
+    # Convert labels for the user to numbers for the backend.
+    leftnamesToNums = {}
+    rightnamesToNums = {}
+    numNameAssign = 0
+    for key in leftNames:
+        leftnamesToNums[key] = numNameAssign
+        numNameAssign += 1
+    numNameAssign = 0
+    for key in rightNames:
+        rightnamesToNums[key] = numNameAssign
+        numNameAssign += 1
+    leftPref = [[rightnamesToNums[pref] for pref in leftNames[i]] for i in leftNames.keys()]
+    rightPref = [[leftnamesToNums[pref] for pref in rightNames[i]] for i in rightNames.keys()]
+    
+    matches = stableMatch(leftPref, rightPref)
+    
+    #Convert backend numbers back to labels
+    for i in range(len(matches)):
+        matches[i][0] = list(leftnamesToNums.keys())[list(leftnamesToNums.values()).index(matches[i][0])]
+        matches[i][1] = list(rightnamesToNums.keys())[list(rightnamesToNums.values()).index(matches[i][1])]
+    return matches
+
 pref1 = [[0,1],[0,0]]
 # A | D, C
 # B | C, C
 pref2 = [[1,0],[1,0]]
 # C | B, A
 # D | B, A
+
+preferences1 = {"Adam" : ["Beth", "Amy", "Diane", "Ellen", "Cara"], 
+                "Bill" : ["Diane", "Beth", "Amy", "Cara", "Ellen"], 
+                "Carl": ["Beth", "Ellen", "Cara", "Diane", "Amy"],
+                "Dan": ["Amy", "Diane", "Cara", "Beth", "Ellen"],
+                "Eric": ["Beth", "Diane", "Amy", "Ellen", "Cara"]}
+preferences2 = {"Amy": ["Eric", "Adam", "Bill","Dan","Carl"],
+                "Beth": ["Carl", "Bill", "Dan", "Adam", "Eric"],
+                "Cara": ["Bill", "Carl", "Dan", "Eric", "Adam"],
+                "Diane": ["Adam", "Eric", "Dan", "Carl", "Bill"],
+                "Ellen": ["Dan", "Bill", "Eric", "Carl", "Adam"]}
 print(stableMatch(pref1,pref2))
-    
+print(stablePreWork(preferences2, preferences1))
